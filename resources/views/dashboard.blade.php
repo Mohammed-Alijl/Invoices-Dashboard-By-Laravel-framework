4abbc271252f2350d9e1d3d32ca5b5f59fb15e98
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-01-27 01:14:22 +0200

    feat: add title for page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.master')
+@section('title')
+    لوحة التحكم
+@stop
 @section('css')
     <!--  Owl-carousel css-->
     <link href="{{URL::asset('assets/plugins/owl-carousel/owl.carousel.css')}}" rel="stylesheet" />
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,5 +1,5 @@
 <!-- Title -->
-<title> Mohammed Alajel Dashboard </title>
+<title> @yield('title') </title>
 <!-- Favicon -->
 <link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
 <!-- Icons css -->
