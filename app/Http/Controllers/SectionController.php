aa5137f4edb769c0dc35782383de300095221b79
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-01-27 20:46:34 +0200

    feat: add section sections and add the add section button

---
+++ app/Http/Controllers/SectionController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SectionRequest;
+use App\Models\Section;
+use Exception;
+use Illuminate\Http\Request;
+
+class SectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('Front-end.sections');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SectionRequest $request)
+    {
+        try {
+            $section = new Section();
+            $section->name = $request->section_name;
+            $section->description = $request->description;
+            if ($section->save()){
+                session()->put('add_section','تم اضافة القسم بنجاح');
+                return redirect()->back();
+            }
+
+        }catch (Exception $ex){
+            return redirect()->back()->withErrors('failed_msg',$ex->getMessage());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Section $section)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Section $section)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Section $section)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Section  $section
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Section $section)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/SectionRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SectionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'section_name'=>'required|unique:sections,name|min:3|max:30',
+            'description'=>'required|max:255'
+        ];
+    }
+    public function messages()
+    {
+        return[
+            'section_name.required'=>'اسم القسم مطلوب',
+            'section_name.unique'=>'هذا القسم موجود بالفعل',
+            'section_name.min'=>'اسم القسم قصير للغاية',
+            'section_name.max'=>'يجب ان يكون اسم القسم اقل من 30 حرف',
+            'description.required'=>'حقل الوصف مطلوب',
+            'description.max'=>'الوصف اطول من اللازم'
+        ];
+    }
+}
