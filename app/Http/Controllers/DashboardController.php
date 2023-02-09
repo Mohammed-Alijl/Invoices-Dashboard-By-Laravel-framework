aa86a9e65055d208310662b46ec95d97e3d97bd0
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-10 01:16:48 +0200

    feat:add new statistical charts in dashboard Page for active & unactive user and for count of paid, unpaid, partially paid invoices in all months in the year

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Invoice;
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
@@ -13,8 +13,17 @@ class DashboardController extends Controller
         $partially_paid_invoices = round(Invoice::where('value_status', '2')->count() / Invoice::count() * 100);
         $paid_invoices = round(Invoice::where('value_status', '3')->count() / Invoice::count() * 100);
 
-        //===============================START BAR CHART===============================
-        $barChart = app()->chartjs
+        $barChart = $this->barChart($unpaid_invoices, $paid_invoices, $partially_paid_invoices);
+        $pieChart = $this->pieChart($unpaid_invoices, $paid_invoices, $partially_paid_invoices);
+        $doughnutChart = $this->doughnutChart();
+        $lineChart = $this->lineChart();
+
+        return view('Front-end.dashboard', compact('barChart', 'pieChart', 'doughnutChart', 'lineChart'));
+    }
+
+    private function barChart($unpaid_invoices, $paid_invoices, $partially_paid_invoices)
+    {
+        return app()->chartjs
             ->name('barChartInvoices')
             ->type('bar')
             ->size(['width' => 200, 'height' => 120])
@@ -24,19 +33,19 @@ class DashboardController extends Controller
                     "label" => "الفواتير الغير مدفوعة",
                     'backgroundColor' => ['#FF597B'], //unpaid, partially paid invoices
                     'data' => [$unpaid_invoices],
-                    'hoverBackgroundColor'=>'#EB455F'
+                    'hoverBackgroundColor' => '#EB455F'
                 ],
                 [
                     "label" => "الفواتير المدفوعة",
                     'backgroundColor' => ['#6ECCAF'], //all, paid invoices
                     'data' => [$paid_invoices],
-                    'hoverBackgroundColor'=>'#68B984'
+                    'hoverBackgroundColor' => '#68B984'
                 ],
                 [
                     "label" => "الفواتير المدفوعة جزئيا",
                     'backgroundColor' => ['#FFB26B'], //all, paid invoices
                     'data' => [$partially_paid_invoices],
-                    'hoverBackgroundColor'=>'#F2921D'
+                    'hoverBackgroundColor' => '#F2921D'
                 ],
             ])
             ->options([
@@ -56,28 +65,128 @@ class DashboardController extends Controller
                     ],
                 ]
             ]);
-//===============================END BAR CHART===============================
+    }
 
-
-
-
-//===============================START PIE CHART=============================
-        $pieChart = app()->chartjs
+    private function pieChart($unpaid_invoices, $paid_invoices, $partially_paid_invoices)
+    {
+        return app()->chartjs
             ->name('pieChartInvoices')
             ->type('pie')
             ->size(['width' => 200, 'height' => 130])
             ->labels(['الفواتير الغير مدفوعة', 'الفواتير المدفوعة', 'الفواتير المدفوعة جزئيا'])
             ->datasets([
                 [
-                    'backgroundColor' => ['#FF597B', '#6ECCAF','#FFB26B'],
-                    'hoverBackgroundColor' => ['#EB455F', '#68B984','#F2921D'],
-                    'data' => [$unpaid_invoices, $paid_invoices ,$partially_paid_invoices]
+                    'backgroundColor' => ['#FF597B', '#6ECCAF', '#FFB26B'],
+                    'hoverBackgroundColor' => ['#EB455F', '#68B984', '#F2921D'],
+                    'data' => [$unpaid_invoices, $paid_invoices, $partially_paid_invoices]
                 ]
             ])
             ->options([]);
-//===============================END PIE CHART===============================
-
-
-        return view('Front-end.dashboard', compact('barChart','pieChart'));
     }
+
+    private function doughnutChart()
+    {
+        $activeUser = round(User::where('status', 1)->count() / User::count() * 100);
+        $unActiveUser = round(User::where('status', 2)->count() / User::count() * 100);
+        return app()->chartjs
+            ->name('donatChartUsers')
+            ->type('doughnut')
+            ->size(['width' => 200, 'height' => 90])
+            ->labels(['المستخدمين المعطلين', 'المستخدمين الفعالين'])
+            ->datasets([
+                [
+                    'label' => 'Users',
+                    'data' => [$activeUser, $unActiveUser],
+                    'backgroundColor' => [
+                        '#FF597B',
+                        'rgb(54, 162, 235)',
+                    ],
+                    'hoverOffset' => 1,
+                ]
+            ])
+            ->options([]);
+    }
+
+    private function lineChart()
+    {
+        $januaryUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '01')->whereYear('invoice_Date', date('Y'))->count();
+        $februaryUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '02')->whereYear('invoice_Date', date('Y'))->count();
+        $marchUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '03')->whereYear('invoice_Date', date('Y'))->count();
+        $aprilUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '04')->whereYear('invoice_Date', date('Y'))->count();
+        $mayUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '05')->whereYear('invoice_Date', date('Y'))->count();
+        $juneUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '06')->whereYear('invoice_Date', date('Y'))->count();
+        $julyUnpaidInvoices = Invoice::where('value_status', 1)->whereMonth('invoice_Date', '07')->whereYear('invoice_Date', date('Y'))->count();
+
+        $januaryPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '01')->whereYear('invoice_Date', date('Y'))->count();
+        $februaryPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '02')->whereYear('invoice_Date', date('Y'))->count();
+        $marchPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '03')->whereYear('invoice_Date', date('Y'))->count();
+        $aprilPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '04')->whereYear('invoice_Date', date('Y'))->count();
+        $mayPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '05')->whereYear('invoice_Date', date('Y'))->count();
+        $junePartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '06')->whereYear('invoice_Date', date('Y'))->count();
+        $julyPartPaidInvoices = Invoice::where('value_status', 2)->whereMonth('invoice_Date', '07')->whereYear('invoice_Date', date('Y'))->count();
+
+        $januaryPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '01')->whereYear('invoice_Date', date('Y'))->count();
+        $februaryPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '02')->whereYear('invoice_Date', date('Y'))->count();
+        $marchPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '03')->whereYear('invoice_Date', date('Y'))->count();
+        $aprilPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '04')->whereYear('invoice_Date', date('Y'))->count();
+        $mayPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '05')->whereYear('invoice_Date', date('Y'))->count();
+        $junePaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '06')->whereYear('invoice_Date', date('Y'))->count();
+        $julyPaidInvoices = Invoice::where('value_status', 3)->whereMonth('invoice_Date', '07')->whereYear('invoice_Date', date('Y'))->count();
+
+
+
+
+        return app()->chartjs
+            ->name('lineChartInvoices')
+            ->type('line')
+            ->size(['width' => 340, 'height' => 200])
+            ->labels(['January', 'February', 'March', 'April', 'May', 'June', 'July'])
+            ->datasets([
+                [
+                    "label" => "الفواتير المدفوعة",
+                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                    'borderColor' => "rgba(38, 185, 154, 0.7)",
+                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [$januaryPaidInvoices, $februaryPaidInvoices, $marchPaidInvoices, $aprilPaidInvoices, $mayPaidInvoices, $junePaidInvoices, $julyPaidInvoices],
+                ],
+                [
+                    "label" => "الفواتير الغير مدفوعة",
+                    'backgroundColor' => "rgba(255, 89, 123, 0.31)",
+                    'borderColor' => "rgba(255, 89, 123 , 0.7)",
+                    "pointBorderColor" => "rgba(255, 89, 123 , 0.7)",
+                    "pointBackgroundColor" => "rgba(255, 89, 123 , 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [$januaryUnpaidInvoices, $februaryUnpaidInvoices, $marchUnpaidInvoices, $aprilUnpaidInvoices, $mayUnpaidInvoices, $juneUnpaidInvoices, $julyUnpaidInvoices],
+                ],
+                [
+                    "label" => "الفواتير المدفوعة جزئيا",
+                    'backgroundColor' => "rgba(242, 146, 29, 0.31)",
+                    'borderColor' => "rgba(242, 146, 29 , 0.7)",
+                    "pointBorderColor" => "rgba(242, 146, 29 , 0.7)",
+                    "pointBackgroundColor" => "rgba(242, 146, 29 , 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => [$januaryPartPaidInvoices, $februaryPartPaidInvoices, $marchPartPaidInvoices, $aprilPartPaidInvoices, $mayPartPaidInvoices, $junePartPaidInvoices, $julyPartPaidInvoices],
+                ]
+
+            ])
+            ->options([
+                'scales' => [
+                    'yAxes' => [
+                        [
+                            'ticks' => [
+                                'min' => 0,
+                            ],
+                        ],
+                    ],
+            ]
+            ]);
+    }
+
+
 }
+
--- resources/views/Front-end/dashboard.blade.php
+++ resources/views/Front-end/dashboard.blade.php
@@ -156,45 +156,29 @@
     <!-- row closed -->
 
     <!-- row opened -->
-{{--    <div class="row row-sm">--}}
-{{--        <div class="col-md-12 col-lg-12 col-xl-7">--}}
-{{--            <div class="card">--}}
-{{--                <div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">--}}
-{{--                    <div class="d-flex justify-content-between">--}}
-{{--                        <h4 class="card-title mb-0">Order status</h4>--}}
-{{--                        <i class="mdi mdi-dots-horizontal text-gray"></i>--}}
-{{--                    </div>--}}
-{{--                    <p class="tx-12 text-muted mb-0">Order Status and Tracking. Track your order from ship date to arrival. To begin, enter your order number.</p>--}}
-{{--                </div>--}}
-{{--                <div class="card-body">--}}
-{{--                    <div class="total-revenue">--}}
-{{--                        <div>--}}
-{{--                            <h4>120,750</h4>--}}
-{{--                            <label><span class="bg-primary"></span>success</label>--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            <h4>56,108</h4>--}}
-{{--                            <label><span class="bg-danger"></span>Pending</label>--}}
-{{--                        </div>--}}
-{{--                        <div>--}}
-{{--                            <h4>32,895</h4>--}}
-{{--                            <label><span class="bg-warning"></span>Failed</label>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div id="bar" class="sales-bar mt-4"></div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-lg-12 col-xl-5">--}}
-{{--            <div class="card card-dashboard-map-one">--}}
-{{--                <label class="main-content-label">Sales Revenue by Customers in USA</label>--}}
-{{--                <span class="d-block mg-b-20 text-muted tx-12">Sales Performance of all states in the United States</span>--}}
-{{--                <div class="">--}}
-{{--                    <div class="vmap-wrapper ht-180" id="vmap2"></div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    <div class="row row-sm">
+        <div class="col-md-12 col-lg-12 col-xl-7">
+            <div class="card">
+                <div class="card-header bg-transparent pd-b-0 pd-t-20 bd-b-0">
+                    <div class="d-flex justify-content-between">
+                        <h4 class="card-title mb-0">مخطط لمستخدمين النظام</h4>
+                        <i class="mdi mdi-dots-horizontal text-gray"></i>
+                    </div>
+                    <p class="tx-12 text-muted mb-0">مخطط يوضح نسبة المستخدمين الفعالين والمستخدمين الغير فعالين</p>
+                </div>
+                {!! $doughnutChart->render() !!}
+            </div>
+        </div>
+        <div class="col-lg-12 col-xl-5">
+            <div class="card card-dashboard-map-one">
+                <label class="main-content-label">الفواتير المضافة خلال الشهور</label>
+                <span class="d-block mg-b-20 text-muted tx-12">مخطط يوضح الفواتير المضافة خلال شهور السنة حيث يظهر عدد الفواتبر المدفوعة والغير مدفوعة والمدفوعة جزئيا المضافة خلال كل شهر</span>
+                <div class="">
+                    {!! $lineChart->render() !!}
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- row closed -->
 
     <!-- row opened -->
