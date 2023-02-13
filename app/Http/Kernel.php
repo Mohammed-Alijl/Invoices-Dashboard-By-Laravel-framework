aab6a58763ede2a78b05be42d805e17638ed371e
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-14 01:27:59 +0200

    feat: setup mcamara package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,10 @@ class Kernel extends HttpKernel
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'localize'                => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+        'localizationRedirect'    => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+        'localeSessionRedirect'   => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+        'localeCookieRedirect'    => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+        'localeViewPath'          => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,35 +19,36 @@ use Illuminate\Support\Facades\Route;
 require __DIR__.'/auth.php';
 
 
-
-Route::middleware('auth')->group(function () {
-    Route::resource('roles', RoleController::class);
-    Route::resource('users', UserController::class);
-    Route::resource('invoices',\App\Http\Controllers\InvoiceController::class);
-    Route::resource('sections',\App\Http\Controllers\SectionController::class);
-    Route::resource('products',\App\Http\Controllers\ProductController::class);
-    Route::resource('attachments',\App\Http\Controllers\AttachmentController::class);
-    Route::resource('payments',\App\Http\Controllers\PaymentController::class);
-    Route::get('/{page}', [\App\Http\Controllers\AdminController::class,'index']);
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/', [\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
-    Route::get('/section/{id}',[\App\Http\Controllers\SectionController::class,'products']);
-    Route::get('paid/invoices',[\App\Http\Controllers\InvoiceController::class,'invoicesPaid'])->name('invoices.paid');
-    Route::get('invoices/not/paid',[\App\Http\Controllers\InvoiceController::class,'invoicesNotPaid'])->name('invoices.not.paid');
-    Route::get('invoices/Partially/paid',[\App\Http\Controllers\InvoiceController::class,'invoicesPartiallyPaid'])->name('invoices.Partially.paid');
-    Route::get('deleted/invoices',[\App\Http\Controllers\InvoiceController::class,'deletedInvoices'])->name('invoices.deleted');
-    Route::get('recovery/invoices/{id}',[\App\Http\Controllers\InvoiceController::class,'recovery'])->name('invoices.recovery');
-    Route::delete('archive/invoices',[\App\Http\Controllers\InvoiceController::class,'archive'])->name('invoices.archive');
-    Route::get('print/invoices/{id}',[\App\Http\Controllers\InvoiceController::class,'print'])->name('invoices.print');
-    Route::get('reports/invoices',[\App\Http\Controllers\InvoiceReportController::class,'index'])->name('reports.invoices');
-    Route::post('search/invoices',[\App\Http\Controllers\InvoiceReportController::class,'search'])->name('invoices.search');
-    Route::get('reports/customers',[\App\Http\Controllers\CustomerReportController::class,'index'])->name('reports.customers');
-    Route::post('search/customers',[\App\Http\Controllers\CustomerReportController::class,'search'])->name('customers.search');
-    Route::get('notification/mark/all',[\App\Http\Controllers\NotificationController::class,'markAllAsRead'])->name('notification.mark.all');
-    Route::get('notification/display/{id}',[\App\Http\Controllers\NotificationController::class,'display'])->name('notification.display');
-    Route::get('notification/real_time/display/{id}',[\App\Http\Controllers\NotificationController::class,'displayRealTime'])->name('notification.display.real.time');
-});
-
-
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'auth']
+    ], function(){
+        Route::resource('roles', RoleController::class);
+        Route::resource('users', UserController::class);
+        Route::resource('invoices', \App\Http\Controllers\InvoiceController::class);
+        Route::resource('sections', \App\Http\Controllers\SectionController::class);
+        Route::resource('products', \App\Http\Controllers\ProductController::class);
+        Route::resource('attachments', \App\Http\Controllers\AttachmentController::class);
+        Route::resource('payments', \App\Http\Controllers\PaymentController::class);
+        Route::get('/{page}', [\App\Http\Controllers\AdminController::class, 'index']);
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/section/{id}', [\App\Http\Controllers\SectionController::class, 'products']);
+        Route::get('paid/invoices', [\App\Http\Controllers\InvoiceController::class, 'invoicesPaid'])->name('invoices.paid');
+        Route::get('invoices/not/paid', [\App\Http\Controllers\InvoiceController::class, 'invoicesNotPaid'])->name('invoices.not.paid');
+        Route::get('invoices/Partially/paid', [\App\Http\Controllers\InvoiceController::class, 'invoicesPartiallyPaid'])->name('invoices.Partially.paid');
+        Route::get('deleted/invoices', [\App\Http\Controllers\InvoiceController::class, 'deletedInvoices'])->name('invoices.deleted');
+        Route::get('recovery/invoices/{id}', [\App\Http\Controllers\InvoiceController::class, 'recovery'])->name('invoices.recovery');
+        Route::delete('archive/invoices', [\App\Http\Controllers\InvoiceController::class, 'archive'])->name('invoices.archive');
+        Route::get('print/invoices/{id}', [\App\Http\Controllers\InvoiceController::class, 'print'])->name('invoices.print');
+        Route::get('reports/invoices', [\App\Http\Controllers\InvoiceReportController::class, 'index'])->name('reports.invoices');
+        Route::post('search/invoices', [\App\Http\Controllers\InvoiceReportController::class, 'search'])->name('invoices.search');
+        Route::get('reports/customers', [\App\Http\Controllers\CustomerReportController::class, 'index'])->name('reports.customers');
+        Route::post('search/customers', [\App\Http\Controllers\CustomerReportController::class, 'search'])->name('customers.search');
+        Route::get('notification/mark/all', [\App\Http\Controllers\NotificationController::class, 'markAllAsRead'])->name('notification.mark.all');
+        Route::get('notification/display/{id}', [\App\Http\Controllers\NotificationController::class, 'display'])->name('notification.display');
+        Route::get('notification/real_time/display/{id}', [\App\Http\Controllers\NotificationController::class, 'displayRealTime'])->name('notification.display.real.time');
+    });
