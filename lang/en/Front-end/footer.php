cf0fb31d384fad8512edc4d5a8efe6d27d140c4a
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-14 16:06:08 +0200

    feat: translate the footer

---
+++ lang/en/Front-end/footer.php
@@ -0,0 +1,5 @@
+<?php
+
+return[
+    'copyright'=>'All programming and design rights reserved ©' . date('Y') . ' '
+];
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer opened -->
 	<div class="main-footer ht-40">
 		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>Copyright © 2023 <a href="https://www.linkedin.com/in/mohammed-alajel/">Mohammed Alajel</a>. Designed by <a href="https://www.linkedin.com/in/mohammed-alajel/">Mohammed Alajel</a> All rights reserved.</span>
+			<span>{{__('Front-end/footer.copyright')}}<a href="https://www.linkedin.com/in/mohammed-alajel/">Mohammed Alajel</a></span>
 		</div>
 	</div>
 <!-- Footer closed -->
