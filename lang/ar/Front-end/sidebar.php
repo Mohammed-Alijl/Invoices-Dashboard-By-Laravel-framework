0ce8dbf905312ed4ed2072df3efe8d52d4b697f7
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-14 15:44:36 +0200

    feat: translate the sidebar

---
+++ lang/ar/Front-end/sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+
+return [
+    'invoices.program'=>'برنامج الفواتير',
+    'dashboard'=>'لوحة التحكم',
+    'invoices'=>'الفواتير',
+    'invoices.list'=>'قائمة الفواتير',
+    'paid.invoices.list'=>'قائمة الفواتير المدفوعة',
+    'unpaid.invoices.list'=>'قائمة الفواتير الغير مدفوعة',
+    'partially.paid.invoices.list'=>'قائمة الفواتير المدفوعة جزئيا',
+    'archive.invoices.list'=>'أرشيف الفواتير',
+    'reports'=>'التقارير',
+    'invoices.reports'=>'تقرير الفواتير',
+    'customers.reports'=>'تقرير العملاء',
+    'users'=>'المستخدمين',
+    'users.list'=>'قائمة المستخدمين',
+    'users.permissions'=>'صلاحيات المستخدمين',
+    'settings'=>'الاعدادات',
+    'sections.list'=>'قائمة البنوك',
+    'products.list'=>'قائمة المعاملات',
+];
---
+++ lang/en/Front-end/sidebar.php
@@ -0,0 +1,22 @@
+<?php
+
+
+return [
+    'invoices.program' => 'Invoices Program',
+    'dashboard' => 'Dashboard',
+    'invoices' => 'Invoices',
+    'invoices.list' => 'Invoices List',
+    'paid.invoices.list' => 'Paid Invoices List',
+    'unpaid.invoices.list' => 'Unpaid Invoices List',
+    'partially.paid.invoices.list' => 'Partially Paid Invoices List',
+    'archive.invoices.list' => 'Invoices Archive',
+    'reports' => 'Reports',
+    'invoices.reports' => 'Invoices Reports',
+    'customers.reports' => 'Customers Reports',
+    'users' => 'Users',
+    'users.list' => 'Users List',
+    'users.permissions' => 'Users Permissions',
+    'settings' => 'Settings',
+    'sections.list' => 'Banks List',
+    'products.list' => 'Transactions List',
+];
