c76cd5cc14ae137d54246e74570d8848f9dba10f
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-14 02:18:04 +0200

    feat: if the user select lang arabic the system is rtl else the system is ltr

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -9,6 +9,7 @@
 <!--  Sidebar css -->
 <link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
 <!-- Sidemenu css -->
+@if(\Illuminate\Support\Facades\App::getLocale() == 'ar')
 <link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
 @yield('css')
 <!--- Style css -->
@@ -17,3 +18,13 @@
 <link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
 <!---Skinmodes css-->
 <link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+@else
+    <link rel="stylesheet" href="{{URL::asset('assets/css/sidemenu.css')}}">
+    @yield('css')
+    <!--- Style css -->
+    <link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
+    <!--- Dark-mode css -->
+    <link href="{{URL::asset('assets/css/style-dark.css')}}" rel="stylesheet">
+    <!---Skinmodes css-->
+    <link href="{{URL::asset('assets/css/skin-modes.css')}}" rel="stylesheet">
+@endif
