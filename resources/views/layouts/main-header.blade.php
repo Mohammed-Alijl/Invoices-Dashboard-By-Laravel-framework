6609bd58f881eeff9a8f8666d3b2fe8f3ca2aaef
Mohammed-Alajel <moh.wa.2016@gmail.com>
2023-02-20 15:49:39 +0200

    fix: delete messages in header

--- resources/views/layouts/main-header.blade.php
+++ resources/views/layouts/main-header.blade.php
@@ -56,83 +56,83 @@
 									</div>
 								</form>
 							</div>
-							<div class="dropdown nav-item main-header-message ">
-								<a class="new nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><span class=" pulse-danger"></span></a>
-								<div class="dropdown-menu">
-									<div class="menu-header-content bg-primary text-right">
-										<div class="d-flex">
-											<h6 class="dropdown-title mb-1 tx-15 text-white font-weight-semibold">Messages</h6>
-											<span class="badge badge-pill badge-warning mr-auto my-auto float-left">Mark All Read</span>
-										</div>
-										<p class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12 ">You have 4 unread messages</p>
-									</div>
-									<div class="main-message-list chat-scroll">
-										<a href="#" class="p-3 d-flex border-bottom">
-											<div class="  drop-img  cover-image  " data-image-src="{{URL::asset('assets/img/faces/3.jpg')}}">
-												<span class="avatar-status bg-teal"></span>
-											</div>
-											<div class="wd-90p">
-												<div class="d-flex">
-													<h5 class="mb-1 name">Petey Cruiser</h5>
-												</div>
-												<p class="mb-0 desc">I'm sorry but i'm not sure how to help you with that......</p>
-												<p class="time mb-0 text-left float-right mr-2 mt-2">Mar 15 3:55 PM</p>
-											</div>
-										</a>
-										<a href="#" class="p-3 d-flex border-bottom">
-											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/2.jpg')}}">
-												<span class="avatar-status bg-teal"></span>
-											</div>
-											<div class="wd-90p">
-												<div class="d-flex">
-													<h5 class="mb-1 name">Jimmy Changa</h5>
-												</div>
-												<p class="mb-0 desc">All set ! Now, time to get to you now......</p>
-												<p class="time mb-0 text-left float-right mr-2 mt-2">Mar 06 01:12 AM</p>
-											</div>
-										</a>
-										<a href="#" class="p-3 d-flex border-bottom">
-											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/9.jpg')}}">
-												<span class="avatar-status bg-teal"></span>
-											</div>
-											<div class="wd-90p">
-												<div class="d-flex">
-													<h5 class="mb-1 name">Graham Cracker</h5>
-												</div>
-												<p class="mb-0 desc">Are you ready to pickup your Delivery...</p>
-												<p class="time mb-0 text-left float-right mr-2 mt-2">Feb 25 10:35 AM</p>
-											</div>
-										</a>
-										<a href="#" class="p-3 d-flex border-bottom">
-											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/12.jpg')}}">
-												<span class="avatar-status bg-teal"></span>
-											</div>
-											<div class="wd-90p">
-												<div class="d-flex">
-													<h5 class="mb-1 name">Donatella Nobatti</h5>
-												</div>
-												<p class="mb-0 desc">Here are some products ...</p>
-												<p class="time mb-0 text-left float-right mr-2 mt-2">Feb 12 05:12 PM</p>
-											</div>
-										</a>
-										<a href="#" class="p-3 d-flex border-bottom">
-											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/5.jpg')}}">
-												<span class="avatar-status bg-teal"></span>
-											</div>
-											<div class="wd-90p">
-												<div class="d-flex">
-													<h5 class="mb-1 name">Anne Fibbiyon</h5>
-												</div>
-												<p class="mb-0 desc">I'm sorry but i'm not sure how...</p>
-												<p class="time mb-0 text-left float-right mr-2 mt-2">Jan 29 03:16 PM</p>
-											</div>
-										</a>
-									</div>
-									<div class="text-center dropdown-footer">
-										<a href="text-center">VIEW ALL</a>
-									</div>
-								</div>
-							</div>
+{{--							<div class="dropdown nav-item main-header-message ">--}}
+{{--								<a class="new nav-link" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="header-icon-svgs" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><span class=" pulse-danger"></span></a>--}}
+{{--								<div class="dropdown-menu">--}}
+{{--									<div class="menu-header-content bg-primary text-right">--}}
+{{--										<div class="d-flex">--}}
+{{--											<h6 class="dropdown-title mb-1 tx-15 text-white font-weight-semibold">Messages</h6>--}}
+{{--											<span class="badge badge-pill badge-warning mr-auto my-auto float-left">Mark All Read</span>--}}
+{{--										</div>--}}
+{{--										<p class="dropdown-title-text subtext mb-0 text-white op-6 pb-0 tx-12 ">You have 4 unread messages</p>--}}
+{{--									</div>--}}
+{{--									<div class="main-message-list chat-scroll">--}}
+{{--										<a href="#" class="p-3 d-flex border-bottom">--}}
+{{--											<div class="  drop-img  cover-image  " data-image-src="{{URL::asset('assets/img/faces/3.jpg')}}">--}}
+{{--												<span class="avatar-status bg-teal"></span>--}}
+{{--											</div>--}}
+{{--											<div class="wd-90p">--}}
+{{--												<div class="d-flex">--}}
+{{--													<h5 class="mb-1 name">Petey Cruiser</h5>--}}
+{{--												</div>--}}
+{{--												<p class="mb-0 desc">I'm sorry but i'm not sure how to help you with that......</p>--}}
+{{--												<p class="time mb-0 text-left float-right mr-2 mt-2">Mar 15 3:55 PM</p>--}}
+{{--											</div>--}}
+{{--										</a>--}}
+{{--										<a href="#" class="p-3 d-flex border-bottom">--}}
+{{--											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/2.jpg')}}">--}}
+{{--												<span class="avatar-status bg-teal"></span>--}}
+{{--											</div>--}}
+{{--											<div class="wd-90p">--}}
+{{--												<div class="d-flex">--}}
+{{--													<h5 class="mb-1 name">Jimmy Changa</h5>--}}
+{{--												</div>--}}
+{{--												<p class="mb-0 desc">All set ! Now, time to get to you now......</p>--}}
+{{--												<p class="time mb-0 text-left float-right mr-2 mt-2">Mar 06 01:12 AM</p>--}}
+{{--											</div>--}}
+{{--										</a>--}}
+{{--										<a href="#" class="p-3 d-flex border-bottom">--}}
+{{--											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/9.jpg')}}">--}}
+{{--												<span class="avatar-status bg-teal"></span>--}}
+{{--											</div>--}}
+{{--											<div class="wd-90p">--}}
+{{--												<div class="d-flex">--}}
+{{--													<h5 class="mb-1 name">Graham Cracker</h5>--}}
+{{--												</div>--}}
+{{--												<p class="mb-0 desc">Are you ready to pickup your Delivery...</p>--}}
+{{--												<p class="time mb-0 text-left float-right mr-2 mt-2">Feb 25 10:35 AM</p>--}}
+{{--											</div>--}}
+{{--										</a>--}}
+{{--										<a href="#" class="p-3 d-flex border-bottom">--}}
+{{--											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/12.jpg')}}">--}}
+{{--												<span class="avatar-status bg-teal"></span>--}}
+{{--											</div>--}}
+{{--											<div class="wd-90p">--}}
+{{--												<div class="d-flex">--}}
+{{--													<h5 class="mb-1 name">Donatella Nobatti</h5>--}}
+{{--												</div>--}}
+{{--												<p class="mb-0 desc">Here are some products ...</p>--}}
+{{--												<p class="time mb-0 text-left float-right mr-2 mt-2">Feb 12 05:12 PM</p>--}}
+{{--											</div>--}}
+{{--										</a>--}}
+{{--										<a href="#" class="p-3 d-flex border-bottom">--}}
+{{--											<div class="drop-img cover-image" data-image-src="{{URL::asset('assets/img/faces/5.jpg')}}">--}}
+{{--												<span class="avatar-status bg-teal"></span>--}}
+{{--											</div>--}}
+{{--											<div class="wd-90p">--}}
+{{--												<div class="d-flex">--}}
+{{--													<h5 class="mb-1 name">Anne Fibbiyon</h5>--}}
+{{--												</div>--}}
+{{--												<p class="mb-0 desc">I'm sorry but i'm not sure how...</p>--}}
+{{--												<p class="time mb-0 text-left float-right mr-2 mt-2">Jan 29 03:16 PM</p>--}}
+{{--											</div>--}}
+{{--										</a>--}}
+{{--									</div>--}}
+{{--									<div class="text-center dropdown-footer">--}}
+{{--										<a href="text-center">VIEW ALL</a>--}}
+{{--									</div>--}}
+{{--								</div>--}}
+{{--							</div>--}}
                             @can('notifications')
 							<div class="dropdown nav-item main-header-notification">
 								<a class="new nav-link" id="bell" href="#">
